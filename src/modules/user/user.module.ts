e6a7b51772006929efcfaf97f4e7ab91cbd47450
Mohamedawad7 <mwd69596@gmail.com>
2026-05-12 13:55:33 +0300

    updates

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { HashingService } from 'src/common';
+import { SeedService } from './seeding.service';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService,HashingService],
+  providers: [UserService,HashingService,SeedService],
 })
 export class UserModule {}
